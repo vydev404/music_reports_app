72da6b3a94cef08e15b92f37d12570ce611133cb
vydev404 <v.yaremenko.dev@gmail.com>
2025-02-18 07:33:41 +0200

    add Config model_config

--- backend/core/config.py
+++ backend/core/config.py
@@ -14,14 +14,23 @@ class ApiPrefixConfig(BaseModel):
 
 class DatabaseConfig(BaseModel):
     url: PostgresDsn #= f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-    echo: bool = True,
-    echo_pool: bool = False,
-    pool_size: int = 10,
-    max_overflow: int = 10,
+    echo: bool = True
+    echo_pool: bool = False
+    pool_size: int = 10
+    max_overflow: int = 10
+
+    def get_url(self):
+        return str(self.url)
 
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        case_sensitive=False,
+        env_prefix="APP__",
+        env_nested_delimiter="__"
+    )
     app: AppRunConfig = AppRunConfig()
     api: ApiPrefixConfig = ApiPrefixConfig()
     db: DatabaseConfig
