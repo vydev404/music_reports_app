a9b2333fe71710faf4f211f568bab4fec959941b
vydev404 <v.yaremenko.dev@gmail.com>
2025-02-19 21:48:21 +0200

    Modified Music fields params

--- backend/core/models/music.py
+++ backend/core/models/music.py
@@ -1,5 +1,6 @@
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
+
 from core.models import Base
 
 
@@ -8,8 +9,8 @@ class Music(Base):
 
     name: Mapped[str] = mapped_column(String(255), nullable=False, )
     title: Mapped[str] =  mapped_column(String(64), nullable=False)
-    artist: Mapped[str] = mapped_column(String(64), nullable=False)
-    album: Mapped[str] = mapped_column(String(64), nullable=False)
+    artist: Mapped[str] = mapped_column(String(64), nullable=True)
+    album: Mapped[str] = mapped_column(String(64), nullable=True)
     right_holder: Mapped[str] = mapped_column(String(64), nullable=False)
     path: Mapped[str] = mapped_column(String(255), nullable=False)
     is_alive: Mapped[bool]
