66b2ec5cc226a5f14ab6becb42203947d88b93ac
vydev404 <v.yaremenko.dev@gmail.com>
2025-02-19 18:55:44 +0200

    Deleted default routed in main app

--- backend/main.py
+++ backend/main.py
@@ -21,14 +21,5 @@ app.include_router(
 )
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
