f005ce2ca7458381738c2acc7b8cc932cdc5a758
vydev404 <v.yaremenko.dev@gmail.com>
2025-02-19 15:08:41 +0200

    Updated Music model & models\__init__.py

--- backend/core/models/__init__.py
+++ backend/core/models/__init__.py
@@ -1,2 +1,6 @@
 from .base import Base
 from .db_manager import db_manager
+from .report import Report
+from .source_file import SourceFile
+from .music import Music
+__all__ = ["Base", "db_manager"]
--- backend/core/models/music.py
+++ backend/core/models/music.py
@@ -1,15 +1,15 @@
+from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
-
 from core.models import Base
 
 
 class Music(Base):
     __tablename__ = "music_library"
 
-    name: Mapped[str] = mapped_column()
-    title: Mapped[str]
-    artist: Mapped[str]
-    album: Mapped[str]
-    right_holder: Mapped[str]
-    path: Mapped[str]
+    name: Mapped[str] = mapped_column(String(255), nullable=False, )
+    title: Mapped[str] =  mapped_column(String(64), nullable=False)
+    artist: Mapped[str] = mapped_column(String(64), nullable=False)
+    album: Mapped[str] = mapped_column(String(64), nullable=False)
+    right_holder: Mapped[str] = mapped_column(String(64), nullable=False)
+    path: Mapped[str] = mapped_column(String(255), nullable=False)
     is_alive: Mapped[bool]
