33b9ff2c4e1998f9961bd1386f0d317275ee29fd
vydev404 <v.yaremenko.dev@gmail.com>
2025-02-20 20:10:33 +0200

    Base model modified update_at field

--- backend/core/models/base.py
+++ backend/core/models/base.py
@@ -1,9 +1,10 @@
-import uuid
 import datetime
+import uuid
 from typing import Annotated
+
 from sqlalchemy import text
-from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 
 #custom fields
 uuid_pk = Annotated[UUID, mapped_column(UUID(as_uuid=True),primary_key=True, default=uuid.uuid4)]
@@ -16,4 +17,4 @@ class Base(DeclarativeBase):
     __abstract__ = True
     id = Mapped[uuid_pk]
     created_at = Mapped[created_at_field]
-    updated_at_field = Mapped[updated_at_field]
+    updated_at = Mapped[updated_at_field]
