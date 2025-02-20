9551df64d51909815f28aa8fa800092b6a5d4fad
vydev404 <v.yaremenko.dev@gmail.com>
2025-02-20 09:19:21 +0200

    Add ApiResponse schema

---
+++ backend/core/schemas/api_response.py
@@ -0,0 +1,15 @@
+from typing import TypeVar, Generic
+
+from pydantic import BaseModel
+
+PS = TypeVar("PS", bound=BaseModel)
+
+
+# PS - pydantic schema
+
+class APIResponse(BaseModel, Generic[PS]):
+    request_id: str
+    code: int
+    status: str
+    message: str
+    data: PS | None = None
