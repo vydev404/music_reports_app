ab5554527cf8c0cd74f9dc5a568aae51ed0e0d61
vydev404 <v.yaremenko.dev@gmail.com>
2025-02-28 14:57:10 +0200

    Add: middleware

--- backend/main.py
+++ backend/main.py
@@ -3,11 +3,9 @@ from typing import AsyncGenerator
 
 import uvicorn
 from fastapi import FastAPI
-from starlette.middleware import Middleware
-
 from api.routes import router as api_router
 from core.models import db_manager
-from middleware.request_middleware import RequestIDMiddleware
+from middleware.request_middleware import request_id_middleware
 
 
 @asynccontextmanager
@@ -16,12 +14,11 @@ async def lifespan(app: FastAPI) -> AsyncGenerator:
     yield
     await db_manager.dispose()
 
-
-app = FastAPI(lifespan=lifespan,
-              middleware=[Middleware(RequestIDMiddleware)])
+app = FastAPI(lifespan=lifespan,)
 app.include_router(
     api_router,
 )
+app.middleware("http")(request_id_middleware)
 
 
 if __name__ == "__main__":
