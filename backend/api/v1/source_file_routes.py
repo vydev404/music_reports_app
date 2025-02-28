6be560c70ad0af51a9db3c320dd2b9ba7f02b2f2
vydev404 <v.yaremenko.dev@gmail.com>
2025-02-28 14:54:31 +0200

    Add: add format_response func, request in params for request_middleware.py

--- backend/api/v1/source_file_routes.py
+++ backend/api/v1/source_file_routes.py
@@ -10,40 +10,47 @@ from core.services.source_file import SourceFileService
 
 router = APIRouter(prefix="/files", tags=["Source Files"])
 
-# @router.post("/", response_model=SourceFileResponse)
-# async def create(file_data: SourceFileCreate, service: SourceFileService = Depends(source_file_service)):
-#     return await service.create(file_data)
 @router.post("/", response_model=APIResponse[SourceFileResponse])
 async def create(file_data: SourceFileCreate, request: Request, service: SourceFileService = Depends(source_file_service)):
-    result =  await service.create(file_data)
-    return APIResponse(
-        request_id=request.state.request_id,  # Передаємо request_id
-        message="File created successfully",
-        data=result
-    )
-
+    result =   await service.create(file_data)
+    return format_response(request, result)
 @router.get("/{file_id}", response_model=APIResponse[SourceFileResponse])
-async def get_by_id(file_id: UUID, service: SourceFileService = Depends(source_file_service)):
-    return await service.get_by_id(file_id)
-
+async def get_by_id(file_id: UUID, request: Request, service: SourceFileService = Depends(source_file_service)):
+    result =  await service.get_by_id(file_id)
+    return format_response(request, result)
 
 @router.get("/", response_model=APIResponse[SourceFileResponseList])
 async def get_files(
+        request: Request,
         limit: int = Query(100, ge=1, le=1000),
-    offset: int = Query(0, ge=0),
-        last_n: int = Query(0, ge=1, le=50),
-    service: SourceFileService = Depends(source_file_service)
+        offset: int = Query(0, ge=0),
+        last_n: int = Query(0, ge=0, le=50),
+        service: SourceFileService = Depends(source_file_service)
 ):
     if last_n:
-        return await service.get_latest(last_n=last_n)
-    return await service.get_many(limit, offset)
+        result =  await service.get_latest(last_n=last_n)
+    else:
+        result = await service.get_many(limit, offset)
+    return format_response(request, result)
 
 
 @router.put("/{file_id}", response_model=APIResponse[SourceFileResponse])
-async def update(file_id: UUID, updates: SourceFileUpdate, service: SourceFileService = Depends(source_file_service)):
-    return await service.update(file_id, updates)
+async def update(
+        request: Request,
+        file_id: UUID,
+        updates: SourceFileUpdate,
+        service: SourceFileService = Depends(source_file_service)
+):
+    result =  await service.update(file_id, updates)
+    return format_response(request, result)
 
 
 @router.delete("/{file_id}", response_model=APIResponse[SourceFileDelete])
-async def delete(file_id: UUID, service: SourceFileService = Depends(source_file_service)):
-    return await service.delete(file_id)
+async def delete(
+        request: Request,
+        file_id: UUID,
+        service: SourceFileService = Depends(source_file_service)
+):
+    result =  await service.delete(file_id)
+    return format_response(request, result)
+
