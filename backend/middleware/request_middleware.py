690d6ad32e4895b77a96a199508014a75ffc6b09
vydev404 <v.yaremenko.dev@gmail.com>
2025-02-28 14:55:25 +0200

    Mod: request_middleware.py

--- backend/middleware/request_middleware.py
+++ backend/middleware/request_middleware.py
@@ -2,16 +2,15 @@ import logging
 import uuid
 
 from fastapi import Request
-from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.responses import Response
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 
-class RequestIDMiddleware(BaseHTTPMiddleware):
-
-    async def dispatch(self, request: Request, call_next):
-        request_id = str(uuid.uuid4())
-        response = await call_next(request)
-        response.headers["X-Request-ID"] = request_id
-        logging.info(f"Request ID: {request_id} | {request.method} {request.url}")
-        return response
+async def request_id_middleware(request: Request, call_next):
+    request_id = str(uuid.uuid4())
+    request.state.request_id = request_id
+    response: Response = await call_next(request)
+    response.headers["X-Request-ID"] = request_id
+    logging.info(f"Request ID: {request_id} | {request.method} {request.url}")
+    return response
