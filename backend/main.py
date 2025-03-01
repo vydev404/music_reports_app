79b99c3499ad0ca8799ab9837f8db2c76c7777ec
vydev404 <v.yaremenko.dev@gmail.com>
2025-03-01 11:06:57 +0200

    Add: CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,8 @@ from typing import AsyncGenerator
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from api.routes import router as api_router
 from core.models import db_manager
 from middleware.request_middleware import request_id_middleware
@@ -18,8 +20,19 @@ app = FastAPI(lifespan=lifespan,)
 app.include_router(
     api_router,
 )
-app.middleware("http")(request_id_middleware)
+origins = [
+    "http://localhost:5173",  # Vite/React
+    "http://127.0.0.1:5173",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.middleware("http")(request_id_middleware)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
