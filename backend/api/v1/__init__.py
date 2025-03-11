d08197160744c0b73b9e36b6092e59b77416ee3b
vydev404 <v.yaremenko.dev@gmail.com>
2025-03-11 19:50:57 +0200

    Fix musics

--- backend/api/v1/__init__.py
+++ backend/api/v1/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-from fastapi import APIRouter
 from core.config import settings
+from fastapi import APIRouter
+
 from . import music_routes, report_routes, source_file_routes
 
 router = APIRouter(
     prefix=settings.api.prefix,
-    tags=["API_V1"],
 )
 router.include_router(music_routes.router)
 router.include_router(report_routes.router)
