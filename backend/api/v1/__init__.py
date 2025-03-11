f8937a0825afa0b70659ef1d561501369f62306a
vydev404 <v.yaremenko.dev@gmail.com>
2025-03-11 15:36:00 +0200

    Changed routes scheme

--- backend/api/v1/__init__.py
+++ backend/api/v1/__init__.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 from fastapi import APIRouter
+from core.config import settings
+from . import music_routes, report_routes, source_file_routes
 
 router = APIRouter(
-    prefix="/music",
-    tags=["Music"],
+    prefix=settings.api.prefix,
+    tags=["API_V1"],
 )
+router.include_router(music_routes.router)
+router.include_router(report_routes.router)
+router.include_router(source_file_routes.router)
