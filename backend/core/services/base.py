fe55799ba10c2e19fc9be1bf46199e064f4cf7c9
vydev404 <v.yaremenko.dev@gmail.com>
2025-03-12 17:25:07 +0200

    Add methods for BaseService

--- backend/core/services/base.py
+++ backend/core/services/base.py
@@ -1,15 +1,33 @@
 from uuid import UUID
 
-from fastapi import HTTPException
-from sqlalchemy.exc import SQLAlchemyError
-
 from core.repositories.base import AbstractRepository
+from fastapi import HTTPException
+from pydantic import BaseModel
+from sqlalchemy.exc import SQLAlchemyError
 
 
 class BaseService:
 
     def __init__(self, repository: AbstractRepository):
         self.repository = repository
+
+    async def create(self, data: BaseModel) -> BaseModel:
+        pass
+
+    async def get_by_id(self, model_id: int) -> BaseModel:
+        pass
+
+    async def get_many(self, limit: int, offset: int) -> BaseModel:
+        pass
+
+    async def get_latest(self, last_n: int) -> BaseModel:
+        pass
+
+    async def update(self, data: BaseModel) -> BaseModel:
+        pass
+
+    async def delete(self, model_id: int) -> BaseModel:
+        pass
 
     async def exists(self, model_id: int) -> bool:
         try:
