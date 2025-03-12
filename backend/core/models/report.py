92614f5cbd3577e6b6cfdd51e54bbe40bc7d4cad
vydev404 <v.yaremenko.dev@gmail.com>
2025-03-12 17:22:44 +0200

    Mod: Report model

--- backend/core/models/report.py
+++ backend/core/models/report.py
@@ -1,6 +1,7 @@
 import json
 
 from sqlalchemy import ForeignKey, Text, String
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.orm import Mapped, relationship, mapped_column
 
 from core.models import Base
@@ -12,14 +13,27 @@ class Report(Base):
     name: Mapped[str] = mapped_column(String(255), nullable=False)
     file: Mapped[str] = mapped_column(String(255), nullable=False)
 
-    source_file_id: Mapped[int] = mapped_column(ForeignKey("source_files.id", ondelete="CASCADE"))
-    source_file: Mapped["SourceFile"] = relationship("SourceFile", back_populates="reports")
-    used_music_ids: Mapped[str] = mapped_column(Text, nullable=True, server_default="[]")
+    source_file_id: Mapped[int] = mapped_column(
+        ForeignKey("source_files.id", ondelete="CASCADE")
+    )
+    source_file: Mapped["SourceFile"] = relationship(
+        "SourceFile", back_populates="reports"
+    )
 
-    @property
-    def music_ids(self) -> list[int]:
-        return json.loads(self.used_music_ids)
-
-    @music_ids.setter
-    def music_ids(self, value: list[int]):
-        self.used_music_ids = json.dumps(value)
+    used_music: Mapped[list[dict]] = mapped_column(
+        JSONB, nullable=True, server_default="[]"
+    )
+    # format [
+    #     {
+    #         "id": 1,
+    #         "count": 2,
+    #         "duration": "00:02:15",
+    #         "title": "Song A"
+    #     },
+    #     {
+    #         "id": 3,
+    #         "count": 1,
+    #         "duration": "00:01:30",
+    #         "title": "Song B"
+    #     }
+    # ]
