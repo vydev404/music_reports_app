d749f8d5086724760ea8641ca46648e5adf8f846
vydev404 <v.yaremenko.dev@gmail.com>
2025-03-14 16:47:36 +0200

    Add: Task queue repository

---
+++ backend/core/repositories/task_queue.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from sqlalchemy import select
+
+from core.models import db_manager
+from core.models.task_queue import TaskQueue, TaskStatus
+from core.repositories.base import SQLAlchemyRepository
+
+
+class TaskQueueRepository(SQLAlchemyRepository):
+    model = TaskQueue
+
+    async def get_by_status(self, status: TaskStatus):
+        async with db_manager.session_getter() as session:
+            query = select(self.model).where(self.model.status == status)
+            result = await session.execute(query)
+            return result.scalars().all()
