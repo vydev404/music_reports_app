5daead69a9911d5159772d1c81c6591128c6f3e5
vydev404 <v.yaremenko.dev@gmail.com>
2025-03-14 16:19:14 +0200

    alembic migrations

--- backend/core/models/__init__.py
+++ backend/core/models/__init__.py
@@ -3,4 +3,6 @@ from .db_manager import db_manager
 from .report import Report
 from .source_file import SourceFile
 from .music import Music
-__all__ = ["Base", "db_manager", "Report", "SourceFile", "Music"]
+from .task_queue import TaskQueue
+
+__all__ = ["Base", "db_manager", "Report", "SourceFile", "Music", "TaskQueue"]
